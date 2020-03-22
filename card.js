5fb80bd51ec6d57eb30e9d3f79614c30420942c0
Jan Grzybowski <JanGrzybowski@outlook.com>
2020-03-22 02:31:04 +0100

    Archive, Pick, Build moves implementation

--- card.js
+++ card.js
@@ -1,19 +1,27 @@
 function Card() {
     return {
+        cardId: 0,
         type: "", //U - upgrade, C - converter, P - pick, B - build, 'F' - file
         effect: null,
         victoryPoints: 0,
         color: "", //R - Red, B - Black, U - Blue, Y - Yellow, RUBY - Any
-        cost: 0
+        cost: 0,
+        level: 0 //0,1,2,3
     };
 }
 
 function InitialCard() {
-    return Card();
+    let card = Card();
+    card.level = 0;
+    return card;
 }
 
 const Level_I_cards = Array(36).fill(Card());
 const Level_II_cards = Array(36).fill(Card());
 const Level_III_cards = Array(36).fill(Card());
 
-export { Card, Level_I_cards, Level_II_cards, Level_III_cards, InitialCard };
+function CardsList() {
+    return Level_I_cards.concat(Level_II_cards, Level_III_cards);
+}
+
+export { Card, CardsList, InitialCard };
--- gizmos-client/src/game.js
+++ gizmos-client/src/game.js
@@ -1,4 +1,4 @@
-import { Level_I_cards, Level_II_cards, Level_III_cards, InitialCard } from '../../card.js';
+import { CardsList, InitialCard } from '../../card.js';
 
 function SomeoneHas16Machines(G) {
     //TODO check end game condition
@@ -15,6 +15,8 @@ const initialEnergyStorageCapacity = 5;
 const initialArchiveLimit = 1;
 const initialResearchLimit = 3;
 
+const visibleEnergyBallsLimit = 6;
+
 const Gizmos = {
     name: "gizmos",
 
@@ -25,29 +27,165 @@ const Gizmos = {
             .fill("B", 13, 27)
             .fill("U", 26, 40)
             .fill("Y", 39, 52),
-        level_I_cards = Level_I_cards,
-        level_II_cards = Level_II_cards,
-        level_III_cards = Level_III_cards,
-        victoryPoints: Array(playersCount).fill(0),
-        energyStorages: Array(playersCount).fill(
-            Array(initialEnergyStorageCapacity)
-        ),
-        archivesLimit: Array(playersCount).fill(Array(initialArchiveLimit)),
-        searchLimits: Array(playersCount).fill(initialResearchLimit),
-        machines: Array(playersCount).fill(Array(1).fill(InitialCard())),
-        archives: Array(playersCount).fill(Array(0))
+        cards = CardsList,
+
+        playerSetup: (playerID) => ({
+            playerId: playerID,
+            victoryPoints: 0,
+
+            energyStorage: Array(0),
+            archivesLimit: Array(0),
+
+            energyStorageCapacity = initialEnergyStorageCapacity,
+            archiveLimit = initialArchiveLimit,
+            researchLimit = initialResearchLimit,
+
+            machines: Array(1).fill(InitialCard()),
+            archive: Array(0)
+        }),
+
+        plugins: [PluginPlayer],
     }),
 
     moves: {
-        archive(G, ctx, id) {
+        archive(G, ctx, cardId) {
+            const playerState = ctx.player.get();
+            if (playerState.archive.length < playerState.archiveLimit) {
+                let cards = [...G.cards]
+                const selectedCard = cards.find((c) => c.cardId == cardId);
 
+                if (typeof selectedCard === 'undefined')
+                    return;
+
+                // add card to player's archive
+                let archive = [...playerState.archive];
+                archive.push(selectedCard);
+                ctx.player.set({...playerState, archive });
+
+                // remove card from common area
+                cards = cards.filter((c) => c.cardId != cardId);
+                return {...G, cards };
+            }
         },
 
-        pick(G, ctx, id) {},
+        pick(G, ctx, energyIndex) {
+            if (energyIndex < 0 || energyIndex > visibleEnergyBallsLimit)
+                return;
 
-        build(G, ctx, id) {},
+            const playerState = ctx.player.get();
+            if (playerState.energyStorage.length >= playerState.energyStorageCapacity)
+                return;
 
-        research(G, ctx, id) {}
+            let dispenser = [...G.dispenser];
+            let energy = dispenser[energy];
+
+            // add energy to player's storage
+            let energyStorage = [...playerState.energyStorage];
+            energyStorage.push(energy);
+            ctx.player.set({...playerState, energyStorage })
+
+            // remove energy from dispenser
+            dispenser = dispenser.filter((e, i) => i != energyIndex);
+            return {...G, dispenser };
+        },
+
+        buildFromCommon(G, ctx, cardId, paidEnergy) {
+            const playerState = ctx.player.get();
+            const selectedCard = cards.find((c) => c.cardId == cardId)
+            if (typeof selectedCard === 'undefined')
+                return;
+
+            let energyStorage = [...playerState.energyStorage];
+
+            // declared energy can pay
+            let costColors = selectedCard.color.toCharArray();
+            let amountToPay = selectedCard.cost;
+            for (let energy in costColors) {
+                if (payment.hasOwnProperty(energy))
+                    amountToPay -= payment[energy];
+            }
+            if (amountToPay != 0)
+                return;
+
+            // add card to player's machines
+            let removeOneEnergy = function(storage, energy) {
+                let i = storage.indexOf(energy);
+                return storage.slice(0, i) + storage.slice(i + 1, storage.length);
+            }
+            energyStorage = energyStorage.reduce(removeOneEnergy, energyStorage);
+
+            // add card to player's machines
+            let machines = [...playerState.archive];
+            machines.push(selectedCard);
+            ctx.player.set({...playerState, machines, energyStorage });
+
+            // remove card from common area
+            cards = cards.filterzc
+            return {...G, cards };
+        },
+
+        buildFromArchive(G, ctx, cardId, paidEnergy) {
+            const playerState = ctx.player.get();
+            const selectedCard = [...playerState.archive.find((c) => c.cardId == cardId)]
+            if (typeof selectedCard === 'undefined') return;
+
+            let energyStorage = [...playerState.energyStorage];
+
+            // player has declared energy
+            let countOcurrences = function(counters, energy) {
+                if (!counters[energy]) {
+                    counters[energy] = 1;
+                } else {
+                    counters[energy] = counters[energy] + 1;
+                }
+                return counters;
+            }
+
+            let playerEnergy = energyStorage.reduce(countOcurrences, {});
+            let payment = paidEnergy.reduce(countOcurrences, {});
+
+            let playerHasDeclaredEnergy = true;
+            for (let energy in payment) {
+                playerHasDeclaredEnergy = playerHasDeclaredEnergy &&
+                    payment.hasOwnProperty(energy) &&
+                    playerEnergy.hasOwnProperty(energy) &&
+                    (payment[energy] <= playerEnergy[energyEnergy]);
+            }
+
+            if (!playerHasDeclaredEnergy) return;
+
+            // declared energy can pay
+            let costColors = selectedCard.color.toCharArray();
+            let amountToPay = selectedCard.cost;
+            for (let energy in costColors) {
+                if (payment.hasOwnProperty(energy))
+                    amountToPay -= payment[energy];
+            }
+            if (amountToPay != 0)
+                return;
+
+            // add card to player's machines
+            let removeOneEnergy = function(storage, energy) {
+                let i = storage.indexOf(energy);
+                return storage.slice(0, i) + storage.slice(i + 1, storage.length);
+            }
+            energyStorage = energyStorage.reduce(removeOneEnergy, energyStorage);
+
+            let machines = [...playerState.machines];
+            machines.push(selectedCard);
+
+            let archive = [...playerState.archive];
+            archive = archive.filter((c) => c.cardId != cardId);
+
+            ctx.player.set({...playerState, machines, archive, energyStorage });
+
+            return {...G };
+        },
+
+
+        research(G, ctx, id) {
+
+        }
     },
 
     turn: {
