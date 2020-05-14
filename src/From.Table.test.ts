113455f431fd99e6282785080819dbfdb99d4778
Jan Grzybowski <JanGrzybowski@outlook.com>
2020-05-14 22:13:28 +0200

    From.Table tests and implementation

---
+++ src/From.Table.test.ts
@@ -0,0 +1,106 @@
+import { GameS } from "./gameState";
+import { From } from "./from";
+import { TestCard } from "./test/TestCard";
+
+test("Should remove one card with given id from table", () => {
+  //Arrange
+  const G = new GameS({
+    cards: [new TestCard(10, 1), new TestCard(11, 1), new TestCard(12, 1), new TestCard(13, 1), new TestCard(14, 1)],
+    visibleCardsLimits: [0, 2, 2, 2],
+  });
+
+  //Act
+  const [gameStateAfterPick, pickedCards] = From.Table(11)(G);
+
+  //Assert
+  expect(pickedCards).toHaveLength(1);
+  expect(gameStateAfterPick.cards.map((c) => c.cardId)).not.toContain(11);
+});
+
+test("Should not take more cards from table", () => {
+  //Arrange
+  const G = new GameS({
+    cards: [new TestCard(10, 1), new TestCard(11, 1), new TestCard(12, 1), new TestCard(13, 1), new TestCard(14, 1)],
+    visibleCardsLimits: [0, 2, 2, 2],
+  });
+
+  //Act
+  const [gameStateAfterPick] = From.Table(11)(G);
+
+  //Assert
+  expect(gameStateAfterPick.cards).toHaveLength(4);
+  expect(gameStateAfterPick.cards.map((c) => c.cardId)).toContain(10);
+  expect(gameStateAfterPick.cards.map((c) => c.cardId)).toContain(12);
+  expect(gameStateAfterPick.cards.map((c) => c.cardId)).toContain(13);
+  expect(gameStateAfterPick.cards.map((c) => c.cardId)).toContain(14);
+});
+
+test("Does not change order of the other cards", () => {
+  //Arrange
+  const G = new GameS({
+    cards: [new TestCard(10, 1), new TestCard(11, 1), new TestCard(12, 1), new TestCard(13, 1), new TestCard(14, 1)],
+    visibleCardsLimits: [0, 2, 2, 2],
+  });
+
+  //Act
+  const [gameStateAfterPick] = From.Table(11)(G);
+
+  //Assert
+  expect(gameStateAfterPick.cards.map((c) => c.cardId)).toMatchObject([10, 12, 13, 14]);
+});
+
+test("Returns picked card", () => {
+  //Arrange
+  const G = new GameS({
+    cards: [new TestCard(10, 1), new TestCard(11, 1), new TestCard(12, 1), new TestCard(13, 1), new TestCard(14, 1)],
+    visibleCardsLimits: [0, 2, 2, 2],
+  });
+
+  //Act
+  const [, pickedCards] = From.Table(11)(G);
+
+  //Assert
+  expect(pickedCards[0]).toMatchObject(new TestCard(11, 1));
+});
+
+test("Does not modify the original game state", () => {
+  //Arrange
+  const G = new GameS({
+    cards: [new TestCard(10, 1), new TestCard(11, 1), new TestCard(12, 1), new TestCard(13, 1), new TestCard(14, 1)],
+    visibleCardsLimits: [0, 2, 2, 2],
+  });
+  const originalGameState = new GameS({
+    cards: [new TestCard(10, 1), new TestCard(11, 1), new TestCard(12, 1), new TestCard(13, 1), new TestCard(14, 1)],
+    visibleCardsLimits: [0, 2, 2, 2],
+  });
+
+  //Act
+  From.Table(11)(G);
+
+  //Assert
+  expect(G).toMatchObject(originalGameState);
+});
+
+test("Throws Error if card is in the pile", () => {
+  //Arrange
+  const G = new GameS({
+    cards: [new TestCard(10, 1), new TestCard(11, 1), new TestCard(12, 1), new TestCard(13, 1), new TestCard(14, 1)],
+    visibleCardsLimits: [0, 2, 2, 2],
+  });
+
+  //Act & Assert
+  expect(() => From.Table(12)(G)).toThrowError();
+  expect(() => From.Table(13)(G)).toThrowError();
+  expect(() => From.Table(14)(G)).toThrowError();
+});
+
+test("Throws Error if card is not on the table", () => {
+  //Arrange
+  const G = new GameS({
+    cards: [new TestCard(10, 1), new TestCard(11, 1), new TestCard(12, 1), new TestCard(13, 1), new TestCard(14, 1)],
+    visibleCardsLimits: [0, 2, 2, 2],
+  });
+
+  //Act & Assert
+  expect(() => From.Table(18)(G)).toThrowError();
+});
--- src/from.ts
+++ src/from.ts
@@ -12,4 +12,20 @@ export class From {
       return [gAfterPick, pickedCards];
     };
   }
+
+  static Table(cardId: number): CardPicker<GameState> {
+    return (G: GameState): [GameState, ReadonlyArray<CardInfo>] => {
+      const selectedCards = G.cards.filter((c) => c.cardId === cardId);
+      if (selectedCards.length !== 1) throw new Error("Card with given id is not on the table.");
+
+      const selectedCard = selectedCards[0];
+      const isCardVisible =
+        G.cards.slice(0, G.visibleCardsLimits[selectedCard.level]).filter((c) => c.cardId === cardId).length === 1;
+
+      if (!isCardVisible) throw new Error("This move should pick exactly one card and it must be visible.");
+
+      const gAfterPick = new GameS({ ...G, cards: G.cards.filter((c: CardInfo) => c.cardId !== selectedCard.cardId) });
+      return [gAfterPick, selectedCards];
+    };
+  }
 }
