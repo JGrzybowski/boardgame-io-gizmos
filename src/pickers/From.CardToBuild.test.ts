1832c4858dee7b4a519e8b00450e976bd54ea3de
Jan Grzybowski <JanGrzybowski@outlook.com>
2020-06-07 23:43:44 +0200

    Fixed tests of From.CardToBeBuild

--- src/pickers/From.CardToBuild.test.ts
+++ src/pickers/From.CardToBuild.test.ts
@@ -5,7 +5,8 @@ import { From } from "./From";
 test("Card is taken from the built slot", () => {
   //Arrange
   const G = new GameS({
-    cards: [new TestCard(10, 1), new TestCard(11, 1), new TestCard(12, 1), new TestCard(13, 1), new TestCard(14, 1)],
+    visibleCards: [new TestCard(10, 1), new TestCard(11, 1)],
+    pileCards: [new TestCard(12, 1), new TestCard(13, 1), new TestCard(14, 1)],
     cardToBeBuilt: new TestCard(10, 1),
     visibleCardsLimits: [0, 2, 2, 2],
   });
@@ -23,7 +24,8 @@ test("Card is taken from the built slot", () => {
 test("Throws an error if there is no card in the slot", () => {
   //Arrange
   const G = new GameS({
-    cards: [new TestCard(10, 1), new TestCard(11, 1), new TestCard(12, 1), new TestCard(13, 1), new TestCard(14, 1)],
+    visibleCards: [new TestCard(10, 1), new TestCard(11, 1)],
+    pileCards: [new TestCard(12, 1), new TestCard(13, 1), new TestCard(14, 1)],
     cardToBeBuilt: null,
     visibleCardsLimits: [0, 2, 2, 2],
   });
@@ -37,13 +39,15 @@ test("Throws an error if there is no card in the slot", () => {
 test("CanPick does not modify the original game state", () => {
   //Arrange
   const G = new GameS({
-    cards: [new TestCard(10, 1), new TestCard(11, 1), new TestCard(12, 1), new TestCard(13, 1), new TestCard(14, 1)],
+    visibleCards: [new TestCard(10, 1), new TestCard(11, 1)],
+    pileCards: [new TestCard(12, 1), new TestCard(13, 1), new TestCard(14, 1)],
     cardToBeBuilt: new TestCard(10, 1),
     visibleCardsLimits: [0, 2, 2, 2],
   });
 
   const originalGameState = new GameS({
-    cards: [new TestCard(10, 1), new TestCard(11, 1), new TestCard(12, 1), new TestCard(13, 1), new TestCard(14, 1)],
+    visibleCards: [new TestCard(10, 1), new TestCard(11, 1)],
+    pileCards: [new TestCard(12, 1), new TestCard(13, 1), new TestCard(14, 1)],
     cardToBeBuilt: new TestCard(10, 1),
     visibleCardsLimits: [0, 2, 2, 2],
   });
@@ -59,13 +63,15 @@ test("CanPick does not modify the original game state", () => {
 test("Pick does not modify the original game state", () => {
   //Arrange
   const G = new GameS({
-    cards: [new TestCard(10, 1), new TestCard(11, 1), new TestCard(12, 1), new TestCard(13, 1), new TestCard(14, 1)],
+    visibleCards: [new TestCard(10, 1), new TestCard(11, 1)],
+    pileCards: [new TestCard(12, 1), new TestCard(13, 1), new TestCard(14, 1)],
     cardToBeBuilt: new TestCard(10, 1),
     visibleCardsLimits: [0, 2, 2, 2],
   });
 
   const originalGameState = new GameS({
-    cards: [new TestCard(10, 1), new TestCard(11, 1), new TestCard(12, 1), new TestCard(13, 1), new TestCard(14, 1)],
+    visibleCards: [new TestCard(10, 1), new TestCard(11, 1)],
+    pileCards: [new TestCard(12, 1), new TestCard(13, 1), new TestCard(14, 1)],
     cardToBeBuilt: new TestCard(10, 1),
     visibleCardsLimits: [0, 2, 2, 2],
   });
@@ -81,7 +87,8 @@ test("Pick does not modify the original game state", () => {
 test("Sets Card's cost to null", () => {
   //Arrange
   const G = new GameS({
-    cards: [new TestCard(10, 1), new TestCard(11, 1), new TestCard(12, 1), new TestCard(13, 1), new TestCard(14, 1)],
+    visibleCards: [new TestCard(10, 1), new TestCard(11, 1)],
+    pileCards: [new TestCard(12, 1), new TestCard(13, 1), new TestCard(14, 1)],
     cardToBeBuilt: new TestCard(10, 1),
     visibleCardsLimits: [0, 2, 2, 2],
   });
@@ -94,25 +101,4 @@ test("Sets Card's cost to null", () => {
   //Assert
   expect(afterPick.cardToBeBuiltCost).not.toBeUndefined();
   expect(afterPick.cardToBeBuiltCost).toBeNull();
-});
-
-test("Removes only card to be build from cards pile", () => {
-  //Arrange
-  const G = new GameS({
-    cards: [new TestCard(11, 1), new TestCard(12, 1), new TestCard(13, 1), new TestCard(14, 1)],
-    cardToBeBuilt: new TestCard(10, 1),
-    visibleCardsLimits: [0, 2, 2, 2],
-  });
-  const picker = From.CardToBuild();
-
-  //Act
-  expect(picker.canPick(G)).toBeTruthy();
-  const [afterPick] = picker.pick(G);
-
-  //Assert
-  expect(afterPick.cards.map((c) => c.cardId)).not.toContain(10);
-  expect(afterPick.cards.map((c) => c.cardId)).toContain(11);
-  expect(afterPick.cards.map((c) => c.cardId)).toContain(12);
-  expect(afterPick.cards.map((c) => c.cardId)).toContain(13);
-  expect(afterPick.cards.map((c) => c.cardId)).toContain(14);
 });
